757a9c7cb6d99eeab4bb3fbf98cf474f4529ebde
Zeith <me@zeith.org>
2024-06-03 07:47:45 +0300

    More data types to give more freedom to addon devs.

--- src/main/java/org/zeith/thaumicadditions/api/data/datas/GauntletData.java
+++ src/main/java/org/zeith/thaumicadditions/api/data/datas/GauntletData.java
@@ -4,7 +4,13 @@ import net.minecraft.item.ItemStack;
 import org.zeith.thaumicadditions.InfoTAR;
 import org.zeith.thaumicadditions.api.data.DataProviderRegistry;
 import org.zeith.thaumicadditions.api.data.DataType;
+import org.zeith.thaumicadditions.compat.thaumcraft.TARCThaumcraft;
 
+/**
+ * Register for custom {@link net.minecraft.item.Item} to let ThaumicAdditions know your {@link net.minecraft.item.Item} is a gauntlet.
+ *
+ * @see TARCThaumcraft#initGauntlets()
+ */
 public class GauntletData
 {
 	public static final GauntletData INSTANCE = new GauntletData();
--- src/main/java/org/zeith/thaumicadditions/api/data/datas/MirrorData.java
+++ src/main/java/org/zeith/thaumicadditions/api/data/datas/MirrorData.java
@@ -3,7 +3,13 @@ package org.zeith.thaumicadditions.api.data.datas;
 import net.minecraft.util.math.BlockPos;
 import org.zeith.thaumicadditions.InfoTAR;
 import org.zeith.thaumicadditions.api.data.DataType;
+import org.zeith.thaumicadditions.compat.thaumcraft.TARCThaumcraft;
 
+/**
+ * Register for custom {@link net.minecraft.tileentity.TileEntity} classes to let ThaumicAdditions know your tile is a mirror (optionally) linked to another mirror.
+ *
+ * @see TARCThaumcraft#initMirrors()
+ */
 public class MirrorData
 {
 	public static final DataType<MirrorData> TYPE = new DataType<>(InfoTAR.id("mirror_data"), MirrorData.class);
@@ -16,5 +22,15 @@ public class MirrorData
 		this.linked = linked;
 		this.linkPos = linkPos;
 		this.linkDimension = linkDimension;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "MirrorData{" +
+			   "linked=" + linked +
+			   ", linkPos=" + linkPos +
+			   ", linkDimension=" + linkDimension +
+			   '}';
 	}
 }
